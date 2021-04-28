15f8a76bfb264fd02964566c3022f57d5068c474
jie151 <61457751+jie151@users.noreply.github.com>
2021-04-28 22:25:17 +0800

    add Material Kit

--- src/cgu_multiButton.js
+++ src/cgu_multiButton.js
@@ -1,3 +1,4 @@
+import Button from '@material-ui/core/Button';
 const styleArgument = {fontSize: '100px', color:'red'};
 
 const changeText=(event)=>{
@@ -8,7 +9,7 @@ const changeText=(event)=>{
 const MultiButton=(num)=>{
     var output = [];
     for(let i = 1; i<num+1; i++)
-        output.push(<button onClick={changeText}>第{i}個按鍵</button>)
+        output.push(<Button variant = "contained" color = "primary" onClick={changeText}>第{i}個按鍵</Button>)
     return output;
 }
 export default MultiButton;
